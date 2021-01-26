f0ee30722154718bcc5b67e9969dbd530ba15b47
Tom Achache <tna2111@columbia.edu>
2021-01-26 08:13:16 -0800

    Update Noisy_QSS.py

--- Noisy_QSS.py
+++ Noisy_QSS.py
@@ -6,9 +6,11 @@ Created on Mon Jan 25 18:13:18 2021
 @author: tomachache
 """
 
-from qiskit import *
 import numpy as np
 import matplotlib.pyplot as plt
+
+from qiskit import *
+
 from QNN import QNN
 
 
@@ -171,7 +173,7 @@ if __name__ == "__main__":
     
     # Create QAE and load weights
     QAE = QNN([3,1,3])
-    K = np.load('/Users/tomachache/Downloads/Columbia/Spring 2020/Quantum Computing - Theory and Practice/Project/Saved_model/Depolarizing_channel/K-3,1,3. p = 0.2. 150 pairs, eps = 0.1. eta = 0.25. Depolarizing channel.npy')
+    K = np.load('Saved_models/Depolarizing_channel/K-3,1,3. p = 0.2. 150 pairs, eps = 0.1. eta = 0.25. Depolarizing channel.npy')
     QAE.set_K(K)
     
     # Use LaTeX font for plots
